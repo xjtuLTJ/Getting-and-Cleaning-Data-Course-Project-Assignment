bb56307ebd8a1c1c040b4ebfb61fc16968e11f5f
xjtuLTJ <43645774+xjtuLTJ@users.noreply.github.com>
2020-05-10 23:03:30 +0800

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,52 +1,45 @@
-options(stringsAsFactors = F)
+
+
+activity_labels<-read.table("activity_labels.txt")
+features<-read.table("features.txt")
+
+
+test_data<-read.table("test/X_test.txt")
+test_labels<-read.table("test/y_test.txt")
+subject_test<-read.table("test/subject_test.txt")
+train_data<-read.table("train/X_train.txt")
+train_labels<-read.table("train/y_train.txt")
+subject_train<-read.table("train/subject_train.txt")
+names(test_data)<-features$V2
+names(train_data)<-features$V2
+train_data2<-cbind(subject_train,train_labels,train_data)
+test_data2<-cbind(subject_test,test_labels,test_data)
+full_data<-rbind(train_data2,test_data2)
+full_data<-merge(activity_labels,full_data,by.x = 1,by.y = 2)
+full_data<-full_data[,-1]
+names(full_data)[1:2]<-c("activity","subjectid")
+names(full_data)<-tolower(names(full_data))
+
+
+full_data2<-full_data[,c(1,2,(grep("mean\\()|std\\()",names(full_data))))]
+
+
+library(reshape2)
 library(dplyr)
-library(tidyr)
-setwd('E:\\GEO Rwd\\Coursera\\Getting and Cleaning Data\\getdata_projectfiles_UCI HAR Dataset\\UCI HAR Dataset')
 
-train <- read.table("./train/X_train.txt",header = F,check.names = F) %>% tbl_df()
-test <- read.table("./test/X_test.txt",header = F,check.names = F) %>% tbl_df()
-subject_train <- read.table("./train/subject_train.txt",header = F,check.names = F) %>% tbl_df()
-subject_test <- read.table("./test/subject_test.txt",header = F,check.names = F) %>% tbl_df()
-label_train <- read.table("./train/y_train.txt",header = F,check.names = F) %>% tbl_df()
-label_test <- read.table("./test/y_test.txt",header = F,check.names = F) %>% tbl_df()
-
-trainData <- mutate(train, mean = apply(train,1,mean)) %>% mutate(std = apply(train,1,sd)) %>%
-        mutate(dataType = "train")
-testData <- mutate(test, mean = apply(test,1,mean)) %>% mutate(std = apply(test,1,sd)) %>% 
-        mutate(dataType = "test")
-
-names(subject_train) <- "subject"
-names(subject_test) <- "subject"
-names(label_train) <- "label"
-names(label_test) <- "label"
-
-trainData <- cbind(trainData,subject_train,label_train)
-testData <- cbind(testData,subject_test,label_test)
-
-allData <- rbind(trainData,testData)
-
-selectData <- select(allData,mean,std,subject,label,dataType)
-
-groupMean <- select(allData,mean,subject,label,dataType)
-
-write.table(groupMean,file = "groupMean.txt",sep = '\t',quote = F,row.names = F)
+full_data3<-melt(full_data2,id=c("activity","subjectid"))
+full_data3<-dcast(full_data3,activity + subjectid ~variable,mean)
+full_data3<-arrange(full_data3,subjectid,activity)
+final_data<-mutate(full_data3,id=seq.int(nrow(full_data3)))
+final_data<-final_data[,c(69,2,1,3:68)]
+names(final_data)<-gsub("-"," ",names(final_data))
+names(final_data)<-gsub("\\()","",names(final_data))
 
 
+names(final_data)<-gsub("std","standard deviation",names(final_data))
+names(final_data)<-gsub("\\bx\\b","x-axis",names(final_data))
+names(final_data)<-gsub("\\by\\b","y-axis",names(final_data))
+names(final_data)<-gsub("\\bz\\b","z-axis",names(final_data))
 
 
-
-
-trainData <- mutate(train, mean = apply(train,1,mean)) 
-selectTrain <- select(trainData, mean)
-trainTbl <- cbind(selectTrain,subject_train,label_train)
-names(trainTbl)[2:3] <- c("subject", "label")
-
-testData <- mutate(test, mean = apply(test,1,mean)) 
-selectTest <- select(testData, mean)
-testTbl <- cbind(selectTest,subject_test,label_test) 
-names(testTbl)[2:3] <- c("subject", "label")
-
-trainTbl <- mutate(trainTbl, dataType = "train")
-testTbl <- mutate(testTbl, dataType = "test")
-
-allTbl <- rbind(trainTbl,testTbl)
+write.table(final_data,"Tidy_Data.txt", row.name = FALSE)
